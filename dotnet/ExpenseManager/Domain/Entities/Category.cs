e7c456eeacdd8884575a99ef20cc8831f2d4dc6d
dham <fullstackcodingguy@gmail.com>
2025-02-28 18:11:05 +0530

    removed expense tracker api app, moved it into a new repo

--- dotnet/ExpenseManager/Domain/Entities/Category.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace ExpenseManager.Domain.Entities
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
